24d9e1e6d5073a52edd0234f3b1f447d3d256c8b
Richard Nguyen <nguyenphuyfgw@gmail.com>
2025-02-19 18:29:54 +0700

    [CHORE] Configure Navbar

---
+++ src/utils/themes/images.ts
@@ -0,0 +1,9 @@
+import midnightTech from '@/assets/images/logo.png';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  LOGO: {
+    MID_9_TECH: midnightTech,
+  },
+  
+};
